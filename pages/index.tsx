716d9a372abd1975fa09bb149ef7ac486b4589c7
Grigoriy Kolenko <kolenko@me.com>
2022-04-22 18:51:57 +0200

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
+import Favicon from "../components/Favicon"
 
 const Home: NextPage = () => {
-  return <>test</>
+  return (
+    <>
+      <Favicon />
+      <Head>
+        <title>Alex Kucun works?</title>
+        <meta name="description" content="Alex Kucun's not personal website" />
+      </Head>
+      <div className="h-screen w-full flex items-center justify-center">
+        <div>
+          <h1 className="text-2xl">
+            <span className="text-red-500">Alex Kucun</span> works?
+          </h1>
+          <p className="text-gray-500">Of course not! He is a software engineer, and He is not a slave!</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
